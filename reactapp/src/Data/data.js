fdcf3d35e19870402ad5ef79529c2b4a60448b8e
Iamneo <techadmin@iamneo.ai>
2023-08-01 18:27:19 +0000

    Backup Commit

---
+++ reactapp/src/Data/data.js
@@ -0,0 +1,50 @@
+const questions = [
+    {
+      qId: 1,
+      question: "who is the father of our nation ?",
+      option_1: "Mahatma Gandhi",
+      option_2: "Jawaharlal Nehru",
+      option_3: "Donald Trump",
+      option_4: "Barrack Obama",
+      answer: "Mahatma Gandhi",
+    },
+    {
+      qId: 2,
+      question: "What color is are the leaves ?",
+      option_1: "Blue",
+      option_2: "Red",
+      option_3: "Yellow",
+      option_4: "Green",
+      answer: "Green",
+    },
+    {
+      qId: 3,
+      question: "What color is the sky ?",
+      option_1: "Blue",
+      option_2: "Red",
+      option_3: "Yellow",
+      option_4: "Green",
+      answer: "Blue",
+    },
+    {
+      qId: 4,
+      question: "What color is water ?",
+      option_1: "Blue",
+      option_2: "Red",
+      option_3: "Yellow",
+      option_4: "Green",
+      answer: "Blue",
+    },
+    {
+      qId: 5,
+      question: "What color is fire ?",
+      option_1: "Blue",
+      option_2: "Red",
+      option_3: "Yellow",
+      option_4: "Green",
+      answer: "Red",
+    },
+  ];
+  
+  export default questions;
+  
